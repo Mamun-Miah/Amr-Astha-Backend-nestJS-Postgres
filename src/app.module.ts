f3f41e2594283a3b71c9c8ba130613ba0b92852d
Mamun-Miah <mahadyhasen7@gmail.com>
2026-01-19 12:05:02 +0600

    updated auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
