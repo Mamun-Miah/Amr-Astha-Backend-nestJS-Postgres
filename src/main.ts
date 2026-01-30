ced26dee4e23c8dc4f1abb3869b40c8f42abc816
Mamun-Miah <mahadyhasen7@gmail.com>
2026-01-30 13:57:14 +0600

    updated

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,23 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import { GlobalExceptionFilter } from './exceptions/prisma-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
-  app.useLogger(app.get(Logger));
-  app.useGlobalFilters(new GlobalExceptionFilter());
+  // --- Security ---
+  app.use(helmet());
   app.use(cookieParser());
-  app.setGlobalPrefix('api');
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  });
+
+  // --- Logging ---
+  app.useLogger(app.get(Logger));
+
+  // --- Global Filters ---
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  // --- Global Pipes ---
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -26,6 +30,15 @@ async function bootstrap() {
       },
     }),
   );
+
+  // --- API Prefix ---
+  app.setGlobalPrefix('api');
+
+  // --- CORS ---
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.enableShutdownHooks();
   await app.listen(process.env.PORT ?? 5000);
 }
