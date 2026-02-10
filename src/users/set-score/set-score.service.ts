40b90b647ed0323ad3a011993c863b66e4df8101
Mamun-Miah <mahadyhasen7@gmail.com>
2026-02-10 12:48:45 +0600

    updated schema

--- src/users/set-score/set-score.service.ts
+++ src/users/set-score/set-score.service.ts
@@ -8,6 +8,9 @@ export class SetScoreService {
   async setEvidenceScore(orderId: number) {
     //if no evidence provided
     //check evidence has or not
+    if (!orderId) {
+      throw new Error('OrderId not found');
+    }
     const checkEvidence = await this.prisma.orderCreation.findFirst({
       where: {
         id: orderId,
@@ -19,12 +22,45 @@ export class SetScoreService {
         profOfDelivery: true,
       },
     });
-    if (!checkEvidence)
-      return {
-        score: 80,
-      };
+    if (!checkEvidence) {
+      throw new Error('Order not found');
+    }
+    const score =
+      checkEvidence.invoiceUrl || checkEvidence.profOfDelivery ? 100 : 80;
+    ////////////////////////////////////
+    //save score to the main score board
+    //find user id
+    ////////////////////////////////////
+    const userId = await this.prisma.orderCreation.findFirst({
+      where: {
+        id: orderId,
+      },
+      select: {
+        userId: true,
+      },
+    });
+    if (!userId) {
+      throw new Error('User not found');
+    }
+    //save score to seller score
+    await this.prisma.sellerScore.upsert({
+      where: {
+        userId: userId.userId,
+      },
+      create: {
+        userId: userId.userId,
+        // totalScore omitted → default 0
+      },
+      update: {
+        totalScore: {
+          increment: score,
+        },
+      },
+    });
+    ////////////////////////////////////
+    //return score to customer service
     return {
-      score: 100,
+      score,
     };
   }
 }
